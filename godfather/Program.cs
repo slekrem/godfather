8243ee20bef65982fd285c482c36ec4c9a401c15
Stefan Merkel <merkel.s.ulm@gmail.com>
2017-01-12 11:12:43 +0100

    update

--- godfather/Program.cs
+++ godfather/Program.cs
@@ -7,41 +7,40 @@
 
 	class MainClass
 	{
+		private static WebClient _client = new WebClient() 
+		{
+			Proxy = new WebProxy("192.168.178.37:8118")
+		};
+
+
 		public static void Main(string[] args)
 		{
+			_client.Headers[HttpRequestHeader.UserAgent] = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2";
+			
 			var timer = new Timer();
 			timer.Elapsed += DownloadSourceCode;
 			timer.Interval = 5000;//900000;
 			timer.Enabled = true;
 
 			Console.WriteLine("Press \'q\' to quit the sample.");
-			while (Console.Read() != 'q')
-			{
-			}
+			while (Console.Read() != 'q') { }
 		}
 
 		private static void DownloadSourceCode(object source, ElapsedEventArgs e)
 		{
-			DownloadHttp("gibip.de");
+			//DownloadHttp("gibip.de");
 			DownloadHttp("heise.de");
 		}
 
 		private static void DownloadHttp(string host) 
 		{
-			using (var client = new WebClient()
-			{
-				Proxy = new WebProxy("192.168.178.37:8118")
-			})
-			{
-				var dateTime = DateTime.UtcNow;
-				client.Headers[HttpRequestHeader.UserAgent] = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2";
-				client.DownloadFile("http://" + host, "./" + host + "/" + dateTime.ToFileTimeUtc() + ".html");
-				var content = File.ReadAllText("./" + host + "/" + dateTime.ToFileTimeUtc() + ".html");
-				if (string.IsNullOrWhiteSpace(content))
-					Console.WriteLine("ERROR: " + host);
-				else
-					Console.WriteLine("Success: " + host);
-			}
+			var dateTime = DateTime.UtcNow;
+			_client.DownloadFile("http://" + host, "./" + host + "/" + dateTime.ToFileTimeUtc() + ".html");
+			var content = File.ReadAllText("./" + host + "/" + dateTime.ToFileTimeUtc() + ".html");
+			if (string.IsNullOrWhiteSpace(content))
+				Console.WriteLine("ERROR: " + host);
+			else
+				Console.WriteLine("Success: " + host);
 		}
 	}
 }
